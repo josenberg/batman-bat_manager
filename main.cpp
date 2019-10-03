85c74d5f2930178eb6796d12b9f08994f446d6c9
Philipe Godoy <philipe.godoy@dextra-sw.com>
2019-10-03 12:40:17 -0300

    Init

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+int main () 
+{
+    // with no arguments
+    string input;
+    while (cin >> input) {
+         cout << "input: " << input << endl;
+    }
+    return 0;
+}
+
+
+
