eea3eea0f6c40d94493cb469059953c29a8c9246
Gabriela Souza <gabriela.souza@sensedia.com>
2019-10-03 21:20:52 -0300

    primeira tentativa

--- main.cpp
+++ main.cpp
@@ -1,16 +1,72 @@
 #include <iostream>
 #include <string>
 #include <queue>
+#include <cstring>
 
 using namespace std;
+
+queue <pthread_t> northQueue;
+queue <pthread_t> eastQueue;
+queue <pthread_t> southQueue;
+queue <pthread_t> westQueue;
+
+pthread_mutex_t lock;
+
+void* process(void* args){
+    pthread_mutex_lock(&lock);
+
+    for(i=0; i<(0xFFFFFFFF);i++);
+    cout << "jose" << endl;
+    pthread_mutex_unlock(&lock);
+}
 
 int main () 
 {
     // with no arguments
     string input;
-    while (cin >> input) {
-         cout << "input: " << input << endl;
+
+
+    if (pthread_mutex_init(&lock, NULL) != 0)
+    {
+        printf("\n mutex init failed\n");
+        return 1;
     }
+
+    while (cin >> input) {
+
+         cout << "input: " << input << endl;
+
+         for(int i = 0; i < input.length(); i++){
+            if(input[i] == 'n'){
+                northQueue.push(t);
+            }else if(input[i] == 'e'){
+                eastQueue.push(t);
+            }else if(input[i] == 'w'){
+                 westQueue.push(t);
+            }else {
+                 southQueue.push(t);
+            }
+
+         }
+
+         while(!northQueue.empty()){
+            northQueue.pop();
+         }
+         while(!westQueue.empty()){
+             westQueue.pop();
+         }
+         while(!eastQueue.empty()){
+            eastQueue.pop();
+         }
+         while(!southQueue.empty()){
+            southQueue.pop();
+         }
+
+    }
+
+    pthread_mutex_destroy(&lock);
+
+    cout << "a vida eh boa " << endl;
     return 0;
 }
 
